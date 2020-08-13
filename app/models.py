88200f83a9a61820aecf300769a4ff66e5871b9f
magicaljellybeans <tome.robin@gmail.com>
2020-08-13 18:53:17 +0100

    Initialised database and migrations with Post and Tags tables

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from app import login
+from app import login, db
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(UserMixin):
@@ -9,3 +10,26 @@ class User(UserMixin):
 @login.user_loader
 def load_user(id):
     return User()
+
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True))
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author = db.Column(db.String(64))
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+        backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.tag)
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey'
     ADMIN_KEY = os.environ.get('ADMIN_KEY') or 'adminkey'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
