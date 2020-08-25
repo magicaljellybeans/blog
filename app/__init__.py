c02ecc486dceee43d3f385bc4081dc5edddb3ce5
magicaljellybeans <25112739+magicaljellybeans@users.noreply.github.com>
2020-08-25 20:02:38 +0100

    added rotating file handler

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_pagedown import PageDown
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -33,5 +35,18 @@ if not app.debug:
             credentials=auth, secure=secure,timeout=5)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/blog.log',
+                                        maxBytes=10240,
+                                        backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Blog startup')
 
 from app import routes, models, errors
